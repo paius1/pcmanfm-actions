cd28d37e1d722f415d5b028f1a1b482432c0f865
paius1 <paius1@users.noreply.github.com>
2019-04-03 22:54:55 -0500

    Update copy2folder.sh

--- bin/copy2folder.sh
+++ bin/copy2folder.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Original from
+# https://github.com/madebits/linux-pcmanfm-actions
 
 folder=$(yad --file-selection --directory --title="Copy To Folder")
 if [[ $folder ]]; then
 	# cp -r $@ "$folder"
-	for var in "$@"
+	for file in "$@"
 	do
-	    cp -r "$var" "$folder"
+	    cp -r "$file" "$folder"
 	done
 fi
 
+exit 0
